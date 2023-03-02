a190eea7e737109c95b81c30c37dd39b3ec73308
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2023-03-02 19:14:14 +0200

    lecture: getters and setters

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Character {
+    public:
+        Vector2 get_world_pos() { return world_pos; }
+
+    private:
+        Texture2D texture;
+        Texture2D idle;
+        Texture2D run;
+        Vector2 screen_pos;
+        Vector2 world_pos;
+        float right_left{1.f};
+        float current_frame{};
+        float running_time{};
+        float update_time{1.f/12.f};
+};
+
 int main()
 {
     const int window_width = 384;
@@ -18,18 +34,28 @@ int main()
     Vector2 map_pos = {};
     float speed{4.0};
 
-    Texture2D knight = LoadTexture("assets/characters/knight_idle_spritesheet.png");
+    Texture2D knight{};
+    Texture2D knight_run = LoadTexture("assets/characters/knight_run_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("assets/characters/knight_idle_spritesheet.png");
     const float knight_frames = 6.f;
     const float knight_scale = 4.0;
     Vector2 knight_pos{
-        (float) window_width / 2.0f - knight_scale * (0.5f * (float) knight.width / knight_frames),
-        (float) window_height / 2.0f - knight_scale * (0.5f * (float) knight.height)};
+        (float)window_width / 2.0f - knight_scale * (0.5f * (float)knight_run.width / knight_frames),
+        (float)window_height / 2.0f - knight_scale * (0.5f * (float)knight_run.height)};
 
+    // 1: face right, -1: facing left
+    float right_left{1.f};
+    // animation vars
+    float knight_running_time{};
+    float knight_current_frame{};
+    const float knight_update_time{1.f / (fps / knight_frames)};
+
+    float delta{};
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        float delta = GetFrameTime();
+        delta = GetFrameTime();
 
         Vector2 direction{};
         if (IsKeyDown(KEY_A))
@@ -43,16 +69,41 @@ int main()
 
         if (Vector2Length(direction) != 0.0)
         {
-
             map_pos = Vector2Subtract(map_pos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? right_left = -1.f : right_left = 1.f;
+            knight = knight_run;
+        } else {
+            knight = knight_idle;
         }
         DrawTextureEx(map, map_pos, 0.0, 4.0, WHITE);
-        Rectangle src{0.f, 0.f , (float) knight.width/6.f, (float) knight.height};
-        Rectangle dst{knight_pos.x, knight_pos.y, knight_scale * knight.width/knight_frames, knight_scale * knight.height};
+
+        knight_running_time += delta;
+        if (knight_running_time > knight_update_time)
+        {
+            knight_current_frame++;
+            knight_running_time = 0.f;
+            if (knight_current_frame > knight_frames)
+            {
+                knight_current_frame = 0;
+            }
+        }
+
+        Rectangle src{(float)knight_run.width / 6.f * knight_current_frame,
+                      0.f,
+                      right_left * (float)knight_run.width / 6.f,
+                      (float)knight_run.height};
+
+        Rectangle dst{
+            knight_pos.x,
+            knight_pos.y,
+            knight_scale * knight_run.width / knight_frames,
+            knight_scale * knight_run.height};
+        
         DrawTexturePro(knight, src, dst, Vector2{}, 0.f, WHITE);
         EndDrawing();
     }
 
     UnloadTexture(map);
-    UnloadTexture(knight);
+    UnloadTexture(knight_run);
+    UnloadTexture(knight_idle);
 }
